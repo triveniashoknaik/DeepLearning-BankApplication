5d9dff4e21a989e7aaee089b6d769f174b3cca16
Triveni Ashok Naik <triveniashoknaik21@gmail.com>
2017-10-24 18:47:15 -0400

    Update ann.py

--- ann.py
+++ ann.py
@@ -31,7 +31,7 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Part 2 - Now let's make the ANN!
+# Part 2 - Making the the ANN!
 
 # Importing the Keras libraries and packages
 import keras
